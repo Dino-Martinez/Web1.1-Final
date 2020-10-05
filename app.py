90aa9ce6b89c1cc6e4fcdbc8f639a73c212e5205
Secundino Martinez <Dino.Martinez.514@gmail.com>
2020-10-05 15:48:21 -0700

    added os import

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import os
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/userDatabase"
